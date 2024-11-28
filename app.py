bd310cdd9ecd1c20fe53c347d99ec7c1077feaab
Md.Dillshadul Islam <mddillshadul98@gmail.com>
2024-11-29 04:21:52 +0600

    making a deploy ready fixing port file for app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask, request, jsonify
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -41,4 +42,5 @@ def predict():
     return jsonify({'ASD_traits': result})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port, debug=True)
